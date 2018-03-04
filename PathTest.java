9342951d554527a3f6332620a3df6ded1f057e8f
rbschaaf <35942400+rbschaaf@users.noreply.github.com>
2018-03-04 15:46:39 -0700

    Update PathTest.java

--- PathTest.java
+++ PathTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
 * Class used to develop the Path Class
 *Last Edited by Dayan J.
@@ -6,6 +8,19 @@
 
 public class PathTest {
   public static void main(String[] args) {
+    String building = "Taylor Family Digital Library";
+    int roomNum = 161;
+    TestGrid testGrid = new TestGrid();
+    testGrid.setGrid(building, roomNum);
+    int[][] grid = testGrid.getGrid();
+    //path.print(grid);
+    Path path = new Path(grid, 160, 161);
+    ArrayList<Node> nodeList = new ArrayList<Node>();
+    nodeList = path.createNodeArray(grid);
+    //System.out.println(nodeList);
+    ArrayList<Node> nodeDistanceList = new ArrayList<Node>();
+    nodeDistanceList = path.setNodeDistances(nodeList);
+    System.out.println(nodeDistanceList);
 
   }
 }
