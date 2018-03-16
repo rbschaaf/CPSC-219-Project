b149401dfe12f0725877944a73a4d9ac661576da
Nickil13 <36387179+Nickil13@users.noreply.github.com>
2018-03-16 17:40:45 -0600

    Update MapMain.java

--- MapMain.java
+++ MapMain.java
@@ -7,29 +7,15 @@ Last Edited by Nicki Feb 28*/
 import java.util.Scanner;
 
 public class MapMain {
-  private Map mainMap;
-  private Path newPath;
-  //private int startRow; -- ** get these from Path
-  //private int startCol;
-  //private int endRow;
-  //private int endCol;
-
-  // gets these two variables from input to the GUI
   private int roomStart;
   private int roomDest;
 
-  /* Constructors
-  *
-  * Create new Map and Path when running main
-  */
 
-  public MapMain() {
-    mainMap = new Map();
-    newPath = new Path(newMap);
-  }
+  //constructor
+  public MapMain (){
 
+  };
 
-  //user input Methods
   //get starting room from user
   public int getStartRoom() {
     System.out.println("Enter the room number nearest to you: ");
@@ -47,41 +33,45 @@ public class MapMain {
   }
 
 
-/*I just got rid of this for the time being. Not sure if still neeede,
-but method names currently causing errors.*/
-/* public void main(){
+  public void main(){
+    Map newMap = new Map();
     //call method to print the map
-    newMap.print();
+    newMap.printGrid();
     //call method to get starting room from user
-    Map.getStartRoom();
-    //call method to set starting room
-    newPath.setStartLoc(roomStart);
-    //place the number 8 as a marker for the starting room
-    newPath.placeStart();
-    //call method to print map again with starting maker
-    newPath.printGrid();
+    int roomStart = getStartRoom();
+
     //call method to get destination room from user
-    getDestRoom();
-    //call method to set destination room
-    newPath.setDestLoc(roomDest);
+    int roomDest = getDestRoom();
+
+
+    // Make a new floorplan
+    FloorPlans floorPlan = new FloorPlans("Taylor Family Digital Library",roomDest);
+    newMap.setCurrentFloorPlan(floorPlan);
+    // make a new path
+    Path path = new Path(floorPlan.getGrid(),roomStart,roomDest);
+    newMap.setStartValues(floorPlan,roomStart);
+    newMap.setEndValues(floorPlan,roomDest);
+
+
+    //call method to create the path from starting room to destination room.
+    int[][] endGrid = path.createPath();
+    //place the number 8 as a marker for the starting room
+    newMap.placeStart(endGrid);
+    newMap.printGrid();
+
     //place the number 5 as a marker for destination room
-    newPath.placeDest();
-    System.out.println("With destination.");
-    //print the map to show markers for starting and destination rooms
-    newPath.printGrid();
-    System.out.println(" ");
-  //  System.out.println("Current X: "+ newPath.getCurrentX());
-  //  System.out.println("Current Y: "+ newPath.getCurrentY());
-      //call method to create the path from starting room to destination room.
-      newPath.createPath();
+    newMap.placeDest(endGrid);
+    newMap.printGrid();
+
     System.out.println("A new grid: ");
     //print the map again with the number 7 showing a path between the rooms
-    newPath.printGrid();
-  }*/
+    newMap.printGrid();
+  }
 
 
   public static void main(String[] args){
-    MapMain map1 = new MapMain();
-    map1.main();
+    MapMain mapMain = new MapMain();
+    mapMain.main();
+
   }
 }
