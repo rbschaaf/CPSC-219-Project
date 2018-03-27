9111f079a94b9fd7790f266800a9587fc6f6f4b8
dayanjayasuriya <dayan.jayasuriya7@gmail.com>
2018-03-27 13:15:35 -0600

    Node now an extension of tile

--- Node.java
+++ Node.java
@@ -2,9 +2,7 @@
  * A class that contains the data for the points on a grid and makes them usable within arraylists for
  * various tasks
  */
-public class Node {
-  private int xcoord;
-  private int ycoord;
+public class Node extends Tile {
   private boolean startNodeVal = false;
   private boolean endNodeVal = false;
   private double startDistance = Double.POSITIVE_INFINITY;
@@ -19,8 +17,7 @@ public class Node {
    * @param newStartDistance the initial distance from the start point
    */
   public Node(int initX, int initY, double newStartDistance) {
-    setXCoord(initX);
-    setYCoord(initY);
+    super(initX, initY);
     setStartDistance(newStartDistance);
     setStartNodeVal(true);
   }
@@ -33,8 +30,7 @@ public class Node {
    * @param val   boolean value whether it is an end point node or not
    */
   public Node(int initX, int initY, boolean val) {
-    setXCoord(initX);
-    setYCoord(initY);
+    super(initX, initY);
     setEndNodeVal(val);
   }
 
@@ -45,8 +41,7 @@ public class Node {
    * @param initY ycoordinate integer
    */
   public Node(int initX, int initY) {
-    setXCoord(initX);
-    setYCoord(initY);
+    super(initX, initY);
   }
 
   /**
@@ -55,8 +50,7 @@ public class Node {
    * @param copyNode
    */
   public Node(Node copyNode) {
-    setXCoord(copyNode.getXCoord());
-    setYCoord(copyNode.getYCoord());
+    super(copyNode.getXCoord(), copyNode.getYCoord());
     setStartNodeVal(copyNode.getStartNodeVal());
     setEndNodeVal(copyNode.getEndNodeVal());
     setStartDistance(copyNode.getStartDistance());
@@ -64,49 +58,6 @@ public class Node {
   }
 
 
-  /**
-   * Setter method for the xcoordinate
-   *
-   * @param newX an xcoordinate cannot be a negative value
-   */
-  public void setXCoord(int newX) {
-    if (newX >= 0) {
-      xcoord = newX;
-    } else {
-      xcoord = 0;
-    }
-  }
-
-  /**
-   * Getter method for the xcoordinate
-   *
-   * @return an xcoordinate
-   */
-  public int getXCoord() {
-    return xcoord;
-  }
-
-  /**
-   * Setter method for the ycoordinate
-   *
-   * @param newY an ycoordinate cannot be a negative value
-   */
-  public void setYCoord(int newY) {
-    if (newY >= 0) {
-      ycoord = newY;
-    } else {
-      ycoord = 0;
-    }
-  }
-
-  /**
-   * Getter method for the ycoordinate
-   *
-   * @return a yoordinate
-   */
-  public int getYCoord() {
-    return ycoord;
-  }
 
   /**
    * Setter method for whether the node is a start node
@@ -190,29 +141,5 @@ public class Node {
     return newConnectedNode;
   }
 
-  /**
-   * Euclidean distance calculator between nodes
-   *
-   * @param newNode for which to calculate the distance for
-   * @return the distance
-   */
-  public double calcDistance(Node newNode) {
-    int xDistance = xcoord - newNode.getXCoord();
-    int yDistance = ycoord - newNode.getYCoord();
-    return (Math.sqrt((xDistance * xDistance) + (yDistance * yDistance)));
-  }
 
-  /**
-   * Check is the position of another node is the same as this node
-   *
-   * @param newNode for which to compare
-   * @return whether or not they are equivalent in terms of coordinates
-   */
-  public boolean equals(Node newNode) {
-    boolean equality = false;
-    if (newNode.getXCoord() == xcoord && newNode.getYCoord() == ycoord) {
-      equality = true;
-    }
-    return equality;
-  }
 }
--- Tile.java
+++ Tile.java
@@ -1,12 +1,98 @@
 // Last updated Feb 29 by Nicki.
 public class Tile{
-  private int xValue;
-  private int yValue;
+  private int xCoord;
+  private int yCoord;
 
 
-  //constructor with coordinates as parameters
-  public Tile(int anXValue, int aYValue){
-    xValue = anXValue;
-    yValue = aYValue;
+  /**
+   * Constructor used for setting the node with an x and y coordinate as well as the endnode value
+   *
+   * @param initX xcoordinate integer
+   * @param initY ycoordinate integer
+   */
+  public Tile(int initX, int initY) {
+    setXCoord(initX);
+    setYCoord(initY);
+  }
+
+  /**
+   * Copy Constuctor for the node
+   *
+   * @param copyNode
+   */
+  public Tile(Tile copyTile) {
+    setXCoord(copyTile.getXCoord());
+    setYCoord(copyTile.getYCoord());
+  }
+
+  /**
+   * Setter method for the xcoordinate
+   *
+   * @param newX an xcoordinate cannot be a negative value
+   */
+  public void setXCoord(int newX) {
+    if (newX >= 0) {
+      xCoord = newX;
+    } else {
+      xCoord = 0;
+    }
+  }
+
+  /**
+   * Getter method for the xcoordinate
+   *
+   * @return an xcoordinate
+   */
+  public int getXCoord() {
+    return xCoord;
+  }
+
+  /**
+   * Setter method for the ycoordinate
+   *
+   * @param newY an ycoordinate cannot be a negative value
+   */
+  public void setYCoord(int newY) {
+    if (newY >= 0) {
+      yCoord = newY;
+    } else {
+      yCoord = 0;
+    }
+  }
+
+  /**
+   * Getter method for the ycoordinate
+   *
+   * @return a yoordinate
+   */
+  public int getYCoord() {
+    return yCoord;
+  }
+
+  /**
+   * Euclidean distance calculator between tiles
+   *
+   * @param aTile for which to calculate the distance for
+   * @return the distance
+   */
+  public double calcDistance(Tile aTile) {
+    int xDistance = getXCoord() - aTile.getXCoord();
+    int yDistance = getYCoord() - aTile.getYCoord();
+    double eucDistance = Math.sqrt((xDistance * xDistance) + (yDistance * yDistance));
+    return eucDistance;
+  }
+
+  /**
+   * Check is the position of another tile is the same as this tile
+   *
+   * @param aTile for which to compare
+   * @return whether or not they are equivalent in terms of coordinates
+   */
+  public boolean equals(Tile aTile) {
+    boolean equality = false;
+    if (aTile.getXCoord() == getXCoord() && aTile.getYCoord() == getYCoord()) {
+      equality = true;
+    }
+    return equality;
   }
 }
