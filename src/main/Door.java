6dbe36272f738bd097d9f0da3bbb236865c98130
rbschaaf <35942400+rbschaaf@users.noreply.github.com>
2018-04-12 15:39:24 -0600

    Completed documenting. Please review

--- src/main/Door.java
+++ src/main/Door.java
@@ -1,35 +1,56 @@
 import java.io.Serializable;
 
 /**
- *
+ * Class for the doors of the rooms on the floor plans.
+ * The doors are what the path will start from and end at. 
  */
-public class Door implements Serializable{
-  private int doorX;
-  private int doorY;
 
-  // basic constructor
+public class Door implements Serializable{
+  private int doorX; // x-coordinate of the door location
+  private int doorY; // y-coordinate of the door location.
+
+  /**
+  * Default constructor for Door.
+  */
   public Door(){}
 
-  // copy constructor
+  /**
+  * Copy constructor for Door
+  */
   public Door(Door toBeCopied){
     doorX = toBeCopied.getDoorX();
     doorY = toBeCopied.getDoorY();
   }
 
-  // getter & setter methods for doorX
+  /**
+  * Method to get the x-coordinate of the door on the floor plan.
+  * @return: doorX the x-coordinate of the door as an integer.
+  */
   public int getDoorX(){
     return doorX;
   }
+
+  /**
+  * Method to set the x-coordinate of the door on the floor plan.
+  * @param: newDoorX is a x-coordinate to set the location the door as an integer.
+  */
   public void setDoorX(int newDoorX){
     doorX = newDoorX;
   }
 
-  // getter & setter methods for doorY
+  /**
+  * Method to get the y-coordinate of the door on the floor plan.
+  * @return: doorY the y-coordinate of the door as an integer.
+  */
   public int getDoorY(){
     return doorY;
   }
+
+  /**
+  * Method to set the y-coordinate of the door on the floor plan.
+  * @param: newDoorY is a y-coordinate to set the location the door as an integer.
+  */
   public void setDoorY(int newDoorY){
     doorY = newDoorY;
   }
 }
-
