496afef6abb4a854af88d566b660363eeb54e2f3
rbschaaf <35942400+rbschaaf@users.noreply.github.com>
2018-04-13 09:46:31 -0600

    documenting completed by Riley. Please review

--- src/test/DoorTest.java
+++ src/test/DoorTest.java
@@ -1,5 +1,5 @@
 /**
-* Class to test the Door class
+* Test Class of JUnit tests to test the Door class of the room finding program.
 */
 
 import org.junit.Test;
@@ -9,17 +9,26 @@ import static org.junit.Assert.assertTrue;
 
 public class DoorTest{
 
+  /**
+  * Testing the copy constructor for the Door.
+  */
   @Test
   public void testCopyConstructor() {
+    //Construct a door and set the x and y coordinates.
     Door a = new Door();
     a.setDoorX(10);
     a.setDoorY(5);
+    //Copy the previously created door to a new door and compare the x and y coordinates are the same.
     Door b = new Door(a);
     assertTrue("Door's X is not correct", a.getDoorX() == b.getDoorX() && a.getDoorY() == b.getDoorY());
   }
 
+  /**
+  * Test the setting and getting of the x coordinates of the Door.
+  */
   @Test
   public void setAndGetDoorX() {
+    //Create a Door, set the x coordinates for it, and compare with the getter method.
     Door a = new Door();
     a.setDoorX(10);
     a.setDoorY(5);
@@ -33,9 +42,12 @@ public class DoorTest{
   }
 
 
-
+  /**
+  * Test the setting and getting of the y coordinates of the Door.
+  */
   @Test
   public void setAndGetDoorY() {
+    //Create a Door, set the y coordinates for it, and compare with the getter method.
     Door a = new Door();
     a.setDoorY(15);
     assertEquals("Door's Y is not correct", 15, a.getDoorY());
