8606eb9c072b7ac03d63cf983d668f4fe40443d6
rbschaaf <35942400+rbschaaf@users.noreply.github.com>
2018-04-13 10:41:40 -0600

    needs two tests made. Documenting done by riley

--- src/test/BuildingTest.java
+++ src/test/BuildingTest.java
@@ -2,8 +2,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+* Test class of JUnit tests to test the Building class for the room finding app.
+* The Building class has associated FloorPlans with it for each floor.
+*/
+
 public class BuildingTest {
 
+    /**
+    * Testing the name of a constructed building can be gotten.
+    */
     @Test
     public void getName() {
         Building a = new Building("Taylor Family Digital Library");
@@ -14,6 +22,9 @@ public class BuildingTest {
         assertNull("This is not the correct name", c.getName());
     }
 
+    /**
+    * Test that all the correct floors can be added for the building and this list of floors can be gotten.
+    */
     @Test
     public void addFloors() {
         Building a = new Building("Taylor Family Digital Library");
@@ -24,6 +35,9 @@ public class BuildingTest {
         assertEquals("These do not create the same lists", d.getFloorList(), c.getFloorList());
     }
 
+    /**
+    * Test that a specific Floor plan for a building can be gotten based on provided room numbers.
+    */
     @Test
     public void getFloorPlan() {
         Building a = new Building("Taylor Family Digital Library");
@@ -34,12 +48,18 @@ public class BuildingTest {
         assertNotEquals("These do not create the same lists", c.getFloorPlan(199) , d.getFloorPlan(200));
     }
 
+    /**
+    * Test the getter method for the list of floors for a building.
+    */
     @Test
     public void getFloorList() {
 
 
     }
 
+    /**
+    * Test the room provided as a parameter actually exists on a floor in the building.
+    */
     @Test
     public void onAFloor() {
 
