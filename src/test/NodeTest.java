06ba66554416aef21b280b6ba028b14e9801e2c4
dayan.jayasuriya <dayanjayasuriya7@gmail.com>
2018-04-13 06:39:57 -0600

    Editted imports

--- src/test/NodeTest.java
+++ src/test/NodeTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-import static org.junit.Assert.*;
 
 public class NodeTest {
 
