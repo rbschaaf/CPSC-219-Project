064d033bfd28a461474a312beddb6d799bdb964b
dayan.jayasuriya <dayanjayasuriya7@gmail.com>
2018-04-13 02:45:03 -0600

    Door Test functioning

--- src/test/DoorTest.java
+++ src/test/DoorTest.java
@@ -2,44 +2,49 @@
 * Class to test the Door class
 */
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DoorTest{
 
   @Test
-  public void test_getter_and_setter_DoorXandY(){
+  public void testCopyConstructor() {
     Door a = new Door();
     a.setDoorX(10);
     a.setDoorY(5);
-    assertEquals("Door's X is at", 10, a.getDoorX());
-    assertEquals("Door's Y is at", 5, a.getDoorY());
+    Door b = new Door(a);
+    assertTrue("Door's X is not correct", a.getDoorX() == b.getDoorX() && a.getDoorY() == b.getDoorY());
+  }
+
+  @Test
+  public void setAndGetDoorX() {
+    Door a = new Door();
+    a.setDoorX(10);
+    a.setDoorY(5);
+    assertEquals("Door's X is not correct", 10, a.getDoorX());
     Door b = new Door();
     b.setDoorX(0);
-    b.setDoorY(7);
-    assertEquals("Door's X is at", 0, b.getDoorX());
-    assertEquals("Door's Y is at", 7, b.getDoorY());
+    assertEquals("Door's X is not correct", 0, b.getDoorX());
     Door c = new Door();
     c.setDoorX(9);
+    assertEquals("Door's X is not correct", 9, c.getDoorX());
+  }
+
+
+
+  @Test
+  public void setAndGetDoorY() {
+    Door a = new Door();
+    a.setDoorY(15);
+    assertEquals("Door's Y is not correct", 15, a.getDoorY());
+    Door b = new Door();
+    b.setDoorY(20);
+    assertEquals("Door's Y is not correct", 20, b.getDoorY());
+    Door c = new Door();
     c.setDoorY(0);
-    assertEquals("Door's X is at", 9, c.getDoorX());
-    assertEquals("Door's Y is at", 0, c.getDoorY());
+    assertEquals("Door's Y is not correct", 0, b.getDoorY());
   }
 
-  @Test
-  public void getDoorX() {
-  }
-
-  @Test
-  public void setDoorX() {
-  }
-
-  @Test
-  public void getDoorY() {
-  }
-
-  @Test
-  public void setDoorY() {
-  }
 }
