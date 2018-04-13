c32ead38c97ebefc9259496a089546937090f40b
Nickil13 <36387179+Nickil13@users.noreply.github.com>
2018-04-13 10:06:39 -0600

    Update TileTest.java

--- src/test/TileTest.java
+++ src/test/TileTest.java
@@ -5,15 +5,28 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TileTest {
+    
+    /**
+    * Method to test the values set by the tile constructor.
+    *
+    */
     @Test
     public void test_Constructor() {
+        /*Create a new Tile with the x-value at 0 and the y-value at 0. Use getter methods to see if 
+        x-value and y-values returned from the object are the same as those inputted.*/
         Tile n = new Tile(0,0);
         assertEquals("Created Tile(0,0) - testing x value", 0, n.getXCoord());
         assertEquals("Created Tile(0,0) - testing y value", 0, n.getYCoord());
     }
 
+    /**
+    * Method to test when the constructor is given a negative value.
+    *
+    */
     @Test
     public void test_Constructor_Negative() {
+        /* When given negative values, the constructor should set the variable the
+        value was provided for to 0*/
         Tile n = new Tile(-1,1);
         assertEquals("Created Tile(-1,10) - testing x value", 0, n.getXCoord());
         assertEquals("Created Tile(-1,10) - testing y value", 1, n.getYCoord());
@@ -21,10 +34,15 @@ public class TileTest {
 
 
 
-
+    /*
+    * Method to test if the copy constructor sets the new object with appropriate x and y values 
+    * from the tile object it copies.
+    */
     @Test
     public void test_CopyConstructor() {
+        // Create a new tile n.
         Tile n = new Tile(1,2);
+        // Copy the tile n using the copy constructor.
         Tile n2 = new Tile(n);
         assertEquals("Created Tile(1,2) and copied it.  Testing x value of original.", 1, n.getXCoord());
         assertEquals("Created Tile(1,2) and copied it.  Testing y value of original.", 2, n.getYCoord());
@@ -33,56 +51,89 @@ public class TileTest {
     }
 
 
+    /*
+    * Method to test when the x value setter is given 0 as a parameter.
+    *
+    */
     @Test
     public void test_setXCoord_Zero() {
         Tile n = new Tile(5,7);
+        // Set the tile's x coord to 0.
         n.setXCoord(0);
+        // The x value should be equal to 0 and the y value should remain the same.
         assertEquals("Created Tile(5,7) then set xcoord to zero - testing x value", 0, n.getXCoord());
         assertEquals("Created Tile(5,7) then set xcoord to zero - testing y value", 7, n.getYCoord());
     }
 
+    /**
+    * Method to test when the y value setter is given 0 as a parameter.
+    *
+    */
     @Test
     public void test_setYCoord_Zero() {
         Tile n = new Tile(5,7);
+        // Set the tile's y coord to 0.
         n.setYCoord(0);
+        // The x value should remain the same and the y value should be equal to 0.
         assertEquals("Created Tile(5,7) then set ycoord to zero - testing x value", 5, n.getXCoord());
         assertEquals("Created Tile(5,7) then set ycoord to zero - testing y value", 0, n.getYCoord());
     }
 
+    /**
+    * Method to test when the x value is set to a negative integer.
+    *
+    */
     @Test
     public void test_setXCoord_Negative() {
         Tile n = new Tile(5,7);
         n.setXCoord(-54);
+        // The x value should be set to 0 when the setter is given a negative integer as a parameter.
         assertEquals("Created Tile(5,7) then set xcoord to -54 - testing x value", 0, n.getXCoord());
         assertEquals("Created Tile(5,7) then set xcoord to -54 - testing y value", 7, n.getYCoord());
     }
 
+    /**
+    * Method to test when the y value is set to a negative integer.
+    *
+    */
     @Test
     public void test_setYCoord_Negative() {
         Tile n = new Tile(5,7);
         n.setYCoord(-32);
+        // The y value should be set to 0 when the setter is given a negative integer as a parameter.
         assertEquals("Created Tile(5,7) then set ycoord to -32 - testing x value", 5, n.getXCoord());
         assertEquals("Created Tile(5,7) then set ycoord to -32 - testing y value", 0, n.getYCoord());
     }
 
+    /**
+    * Method to test when the x value is given a positive integer as a parameter.
+    *
+    */
     @Test
     public void test_setXCoord_Positive() {
         Tile n = new Tile(5,7);
         n.setXCoord(1);
+        // The x value should be changed to the given parameter, while the y value remains the same.
         assertEquals("Created Tile(5,7) then set xcoord to 1 - testing x value", 1, n.getXCoord());
         assertEquals("Created Tile(5,7) then set xcoord to 1 - testing y value", 7, n.getYCoord());
     }
 
+    /**
+    * Method to test when the y value is given a positive integer as a parameter.
+    *
+    */
     @Test
     public void test_setYCoord_Positive() {
         Tile n = new Tile(5,7);
         n.setYCoord(3);
+         // The x value should be changed to the given parameter, while the y value remains the same.
         assertEquals("Created Tile(5,7) then set ycoord to 3 - testing x value", 5, n.getXCoord());
         assertEquals("Created Tile(5,7) then set ycoord to 3 - testing y value", 3, n.getYCoord());
     }
 
 
     @Test
+    /* Creating two tiles and finding the distance between them should return the correct distance.*/
     public void test_distance_StartIsTopLeft_EndIsBottomRight() {
         Tile n1 = new Tile(1,2);
         Tile n2 = new Tile(4,6);
