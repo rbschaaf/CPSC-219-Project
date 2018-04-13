b115d5de675c2ec9541703ca68ee35583f5ddd13
dayan.jayasuriya <dayanjayasuriya7@gmail.com>
2018-04-13 03:06:33 -0600

    Added Test functional

--- src/test/RoomTest.java
+++ src/test/RoomTest.java
@@ -1,25 +1,34 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class RoomTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void test_ConstructorAndGetter() {
+        Room c = new Room(0);
+        assertEquals("Testing constructor and getter", 0, c.getRoomsNumber());
     }
 
     @Test
-    public void getRoomsNumber() {
+    public void test_addTile_addingOne() {
+        Room c = new Room(10);
+        c.addTile(12,5);
+        ArrayList<Tile> list = c.getTileList();
+        Tile m2 = null;
+        if (list.size() > 0){
+            m2 = list.get(0);
+        }
+        assertEquals("Room only has one Tile - testing size.", 1, list.size());
+        assertEquals("Room only has one Tile - testing x coord.", 12, m2.getXCoord());
+        assertEquals("Room only has one Tile - testing y coord.", 5, m2.getYCoord());
     }
 
     @Test
     public void getTileList() {
+
     }
 
     @Test
