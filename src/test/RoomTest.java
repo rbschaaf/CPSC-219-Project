38e67edf26c183018ae37d47bb6d2e1bcbcff7dc
Nickil13 <36387179+Nickil13@users.noreply.github.com>
2018-04-13 19:23:12 -0600

    Update RoomTest.java

--- src/test/RoomTest.java
+++ src/test/RoomTest.java
@@ -24,13 +24,16 @@ public class RoomTest {
     */
     @Test
     public void test_addTile_addingOne() {
+        // Create a new room and add a tile to it.
         Room c = new Room(10);
         c.addTile(12,5);
+        // If the tile list size is larger than 0, get the Tile at index 0.
         ArrayList<Tile> list = c.getTileList();
         Tile m2 = null;
         if (list.size() > 0){
             m2 = list.get(0);
         }
+        // Check if the list is of size 1 and if the x and y values are valid.
         assertEquals("Room only has one Tile - testing size.", 1, list.size());
         assertEquals("Room only has one Tile - testing x coord.", 12, m2.getXCoord());
         assertEquals("Room only has one Tile - testing y coord.", 5, m2.getYCoord());
@@ -42,6 +45,7 @@ public class RoomTest {
     */
     @Test
     public void getTileList_Encapsulation() {
+        //Create a new room and add tiles to it.
         Room c = new Room(20);
         c.addTile(3, 7);
         c.addTile(0, 7);
@@ -49,9 +53,9 @@ public class RoomTest {
         c.addTile(15, 10);
         c.addTile(12, 9);
         c.addTile(20, 4);
-
+        // Get the tile list from the room and set the x value for each tile to 1.
         ArrayList<Tile> list = c.getTileList();
-
+    
         list.get(0).setXCoord(1);
         list.get(1).setXCoord(1);
         list.get(2).setXCoord(1);
@@ -61,7 +65,7 @@ public class RoomTest {
 
         list = c.getTileList();
 
-
+        // Check if the x coordinate values remain unchanged. If so, the list is properly encapsulated. 
         assertEquals("Tile 1 test - testing x coordinate", 3, list.get(0).getXCoord());
         assertEquals("Tile 2 test - testing x coordinate", 0, list.get(1).getXCoord());
         assertEquals("Tile 3 test - testing x coordinate", 8, list.get(2).getXCoord());
@@ -75,11 +79,12 @@ public class RoomTest {
     */
     @Test
     public void addTile() {
+        // Create a new room and give it tiles.
         Room c = new Room(20);
         c.addTile(3, 7);
         c.addTile(0, 7);
         c.addTile(8, 9);
-
+        // Check if the number of tiles added is equal to the size of the list.
         assertEquals("Check size of ArrayList", 3, c.getTileList().size());
     }
 
@@ -88,9 +93,10 @@ public class RoomTest {
     */
     @Test
     public void getAndSetDoor() {
+        // Create a new room and set its door.
         Room c = new Room(20);
         c.setDoor(40, 50);
-
+        // Verify the x and y dimensions of the door are sucessfully set when accessing the door from the room.
         assertEquals("Check door x dimensions", 40, c.getDoor().getDoorX());
         assertEquals("Check door y dimensions", 50, c.getDoor().getDoorY());
     }
